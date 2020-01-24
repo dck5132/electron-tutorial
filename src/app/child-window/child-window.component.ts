b28faabd87b22e439ca32c9a2533757c1f242e0b
Cyril <Ckalikin@amstedrail.com>
2020-01-24 15:39:28 -0500

    Added new component which will be used in second window + new images added to assets folder

---
+++ src/app/child-window/child-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-window',
+  templateUrl: './child-window.component.html',
+  styleUrls: ['./child-window.component.scss']
+})
+export class ChildWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
